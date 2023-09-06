b1a2a4b11c9b76a34fc3428f63e60bdc1e284bc8
timerdar <timergazin24@mail.ru>
2023-09-06 20:06:16 +0500

    Recreate SpringApp

---
+++ src/main/java/com/timerdar/FamilyFarm/FamilyFarmApplication.java
@@ -0,0 +1,13 @@
+package com.timerdar.FamilyFarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyFarmApplication.class, args);
+	}
+
+}
